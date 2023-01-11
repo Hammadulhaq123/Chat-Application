0338146286137c09d5372d41be091b26fa66123b
Hammadulhaq123 <professorcoding123@gmail.com>
2023-01-11 23:09:05 +0500

    Update Bug fixes in JS and styles updated
    Changed the display of dummy message to none in JS and changed the appearence of the container in CSS also used button instead of icons in html

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ youMsg.style.display = "none";
 function sendFunction(){
     let youChat = document.querySelectorAll('div')[3];
     youChat.style.display = "flex"
-    let otherChat = document.querySelectorAll('div')[4];
-    otherChat.style.display = "flex"
+    // let otherChat = document.querySelectorAll('div')[4];
+    // otherChat.style.display = "flex"
     let text = document.getElementById('textarea').value;
     let div = document.createElement('div');
     div.innerHTML = `${text}`;
     div.setAttribute("class" , "you");
     youChat.appendChild(div);
-    text = document.getElementById('textarea').value = "";
+    document.getElementById('textarea').value = "";
 }
 
 var input = document.getElementById("textarea");
